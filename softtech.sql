948fcabccf7a054764c9ea92f10cb8b7e1331f2e
nafiul1351 <nafiul1351@gmail.com>
2020-12-13 13:16:30 +0600

    second commit

---
+++ softtech.sql
@@ -0,0 +1,385 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 13, 2020 at 07:05 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `softtech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `serialnumber` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `brandname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`id`, `serialnumber`, `brandname`, `created_at`, `updated_at`) VALUES
+(1, '1', 'HP', '2020-12-10 09:35:05', '2020-12-10 09:35:05'),
+(4, '3', 'Intel', '2020-12-12 07:45:22', '2020-12-12 07:45:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `serialnumber` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `categoryname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `serialnumber`, `categoryname`, `created_at`, `updated_at`) VALUES
+(1, '1', 'Processor', '2020-12-10 09:35:56', '2020-12-10 09:35:56'),
+(4, '3', 'Laptop', '2020-12-12 08:28:10', '2020-12-12 08:28:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2020_12_09_055719_create_sellerdetails_table', 1),
+(5, '2020_12_10_054628_create_brands_table', 1),
+(6, '2020_12_10_102256_create_categories_table', 1),
+(13, '2020_12_11_042341_create_shops_table', 2),
+(14, '2020_12_11_163407_create_products_table', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `productid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `productname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `productmodel` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `productcolor` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `productsize` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `coverimage` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `regularprice` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `discountedprice` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `productdescription` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `brand_id` bigint(20) UNSIGNED NOT NULL,
+  `category_id` bigint(20) UNSIGNED NOT NULL,
+  `shop_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `productid`, `productname`, `productmodel`, `productcolor`, `productsize`, `coverimage`, `regularprice`, `discountedprice`, `productdescription`, `user_id`, `brand_id`, `category_id`, `shop_id`, `created_at`, `updated_at`) VALUES
+(2, 'Laptop-SA1', 'HP Probook 450 G4', 'Probook 450 G4', 'Black, Red', 'None', 'images/products/images/1685883645338304.jpg', '56500', '51000', 'Name: HP Probook 450 G4\r\nModel: Probook 450 G4\r\nBrand: HP\r\nIntel Core-i5 6th Generation Processor (2.5GHz to 3.2GHz)\r\n8GB DDR4 RAM\r\n1TB HDD\r\n2GB DDR3 Intel HD Graphics', 2, 1, 4, 1, '2020-12-12 08:38:53', '2020-12-12 08:38:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sellerdetails`
+--
+
+CREATE TABLE `sellerdetails` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `bkashnumber` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `rocketnumber` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sellerdetails`
+--
+
+INSERT INTO `sellerdetails` (`id`, `bkashnumber`, `rocketnumber`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, '01992775545', '019927755453', 2, '2020-12-10 09:37:49', '2020-12-10 09:37:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shops`
+--
+
+CREATE TABLE `shops` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `serialnumber` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `shopname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `shops`
+--
+
+INSERT INTO `shops` (`id`, `serialnumber`, `shopname`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, '1', 'A-Tech', 2, '2020-12-12 08:23:25', '2020-12-12 08:23:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dob` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phonenumber` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'images/users/images/default.png',
+  `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `approved` tinyint(1) NOT NULL DEFAULT 0,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `gender`, `dob`, `email`, `phonenumber`, `image`, `type`, `approved`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Nafiul', 'Islam', 'male', '1995-10-09', 'nafiul1351@gmail.com', '01953806556', 'images/users/images/1685705951677678.jpg', 'Admin', 1, '2020-12-10 09:34:51', '$2y$10$3SuBjqYairwUZ8pAa26Xve6xHMdAdzwzHOKs35unsccubvMqGdJLG', '7Z5kvzd3huoHFUp5Dztqw6m8yuS1xcuSv4rCh9yxoBcdxQBiQj3zMOamHb7U', '2020-12-10 09:34:31', '2020-12-12 09:19:57'),
+(2, 'Sadia', 'Afrin', 'female', '1998-01-01', 'afrin.akter113@gmail.com', '01992775545', 'images/users/images/1685706160032343.jpg', 'Seller', 1, '2020-12-10 09:38:13', '$2y$10$b5Ot/xsm3OIU7I6poiNYveX2tqqi0eSPXAdzQ8NLhhOw2J8m7UR5q', '70FuwxkknPDSAMyDkoAc9FtTZ1s82qydKQEoQCAoJLTxkmZl5Dcell2GMAHs', '2020-12-10 09:37:49', '2020-12-10 22:19:52');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `brands_serialnumber_unique` (`serialnumber`),
+  ADD UNIQUE KEY `brands_brandname_unique` (`brandname`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `categories_serialnumber_unique` (`serialnumber`),
+  ADD UNIQUE KEY `categories_categoryname_unique` (`categoryname`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `products_productid_unique` (`productid`),
+  ADD KEY `products_user_id_foreign` (`user_id`),
+  ADD KEY `products_brand_id_foreign` (`brand_id`),
+  ADD KEY `products_category_id_foreign` (`category_id`),
+  ADD KEY `products_shop_id_foreign` (`shop_id`);
+
+--
+-- Indexes for table `sellerdetails`
+--
+ALTER TABLE `sellerdetails`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `sellerdetails_bkashnumber_unique` (`bkashnumber`),
+  ADD UNIQUE KEY `sellerdetails_rocketnumber_unique` (`rocketnumber`),
+  ADD KEY `sellerdetails_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `shops`
+--
+ALTER TABLE `shops`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `shops_serialnumber_unique` (`serialnumber`),
+  ADD KEY `shops_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD UNIQUE KEY `users_phonenumber_unique` (`phonenumber`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `sellerdetails`
+--
+ALTER TABLE `sellerdetails`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `shops`
+--
+ALTER TABLE `shops`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_brand_id_foreign` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`),
+  ADD CONSTRAINT `products_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `products_shop_id_foreign` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`),
+  ADD CONSTRAINT `products_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `sellerdetails`
+--
+ALTER TABLE `sellerdetails`
+  ADD CONSTRAINT `sellerdetails_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `shops`
+--
+ALTER TABLE `shops`
+  ADD CONSTRAINT `shops_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
